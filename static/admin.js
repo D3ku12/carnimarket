b0ad3e5ef2219fbd6edb83d36213b5c6d2564537
D3ku12 <stevenhm03@gmail.com>
2026-04-22 15:09:26 -0500

    se mejora la diseño en la web

--- static/admin.js
+++ static/admin.js
@@ -487,3 +487,8 @@ async function cargarSelectores() {
 
 // Inicio automático
 window.onload = () => cargarDashboard();
+
+async function logout() {
+    await fetch("/auth/logout", { method: "POST" });
+    window.location.href = "/login";
+}
